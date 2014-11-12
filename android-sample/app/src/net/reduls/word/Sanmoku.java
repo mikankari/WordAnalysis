b4a3d88a2450e5a20892d5a79dd9708e9259e526
Ayu1104 <s1223138@cce.kanagawa-it.ac.jp>
2014-11-13 02:36:38 +0900

    assets_kara_txt_wo_yomu

--- android-sample/app/src/net/reduls/word/Sanmoku.java
+++ android-sample/app/src/net/reduls/word/Sanmoku.java
@@ -1,25 +1,15 @@
 package net.reduls.word;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.KeyEvent;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.LinearLayout;
-import android.util.Log;
-import android.text.Html;
-import android.content.Intent;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.InputMethodManager;
-import android.content.Context;
-import android.text.TextWatcher;
 import android.text.Editable;
 import android.text.SpannableString;
-
-import java.util.List;
-
-import net.reduls.word.R;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Sanmoku extends Activity implements TextWatcher
 {
@@ -52,9 +42,10 @@ public class Sanmoku extends Activity implements TextWatcher
 
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
     public void onTextChanged(CharSequence s, int start, int before, int count) {}
+    
     public void afterTextChanged(Editable s) {
-        //String key = s.toString();
-        String key = "YO! SAY! 夏が胸を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋は やれ爽快 誤魔化し聞かない 薄着の曲線は 確信犯の しなやかなSTYLE 耐水性の 気持ちに切り替わる  瞬間のまぶしさは いかがなもの 心まで脱がされる 熱い風の誘惑に 負けちゃって構わないから 真夏は 不祥事も 君次第で 妖精たちが 夏を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋が できそうかい 君じゃなくても バテ気味にもなるよ 暑いばっかの 街は憂鬱で スキを見せたら 不意に耳に入る サブいギャグなんかで 涼みたくない むせ返る熱帯夜を 彩る花火のように 打ち上げて散る思いなら  このまま抱き合って 焦がれるまで 妖精たちと 夏をしたくなる 熱い欲望はトルネイド 出すものだして 素直になりたい 君と僕となら it`s all right 都会のビルの上じゃ 感じなくなってる君を 冷えたワインの口づけで 酔わせて とろかせて 差し上げましょう 妖精たちが 夏を刺激する 生足ヘソ出しマーメイド 恋にかまけて お留守になるのも ダイスケ的にも オールオーケー YO! SAY! 夏を誰としたくなる 一人根の夜に you can say good bye 奥の方まで 乾く間ないほど 宝物の恋を しま鮮花";
+        String key = s.toString();
+        //String key = "YO! SAY! 夏が胸を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋は やれ爽快 誤魔化し聞かない 薄着の曲線は 確信犯の しなやかなSTYLE 耐水性の 気持ちに切り替わる  瞬間のまぶしさは いかがなもの 心まで脱がされる 熱い風の誘惑に 負けちゃって構わないから 真夏は 不祥事も 君次第で 妖精たちが 夏を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋が できそうかい 君じゃなくても バテ気味にもなるよ 暑いばっかの 街は憂鬱で スキを見せたら 不意に耳に入る サブいギャグなんかで 涼みたくない むせ返る熱帯夜を 彩る花火のように 打ち上げて散る思いなら  このまま抱き合って 焦がれるまで 妖精たちと 夏をしたくなる 熱い欲望はトルネイド 出すものだして 素直になりたい 君と僕となら it`s all right 都会のビルの上じゃ 感じなくなってる君を 冷えたワインの口づけで 酔わせて とろかせて 差し上げましょう 妖精たちが 夏を刺激する 生足ヘソ出しマーメイド 恋にかまけて お留守になるのも ダイスケ的にも オールオーケー YO! SAY! 夏を誰としたくなる 一人根の夜に you can say good bye 奥の方まで 乾く間ないほど 宝物の恋を しま鮮花";
     	LinearLayout resultArea = (LinearLayout)findViewById(R.id.search_result_area);
         resultArea.removeAllViews();
         
@@ -67,6 +58,7 @@ public class Sanmoku extends Activity implements TextWatcher
             }
         }
     }
+    
     //解析ボタンをクリックしたとき
 	private void analysbotton_OnClick(View v) {
         //String key = s.toString();
