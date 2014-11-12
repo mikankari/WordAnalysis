68c7f8ff534c9660f331eb47c6e3c655bf3d8bdb
Ayu1104 <s1223138@cce.kanagawa-it.ac.jp>
2014-11-13 04:05:58 +0900

    kono___TNG!!!

--- android-sample/app/gen/net/reduls/word/R.java
+++ android-sample/app/gen/net/reduls/word/R.java
@@ -1100,6 +1100,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int label=0x7f05003e;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
