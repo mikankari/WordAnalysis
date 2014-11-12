46ec0cf0c652717d4273a3cd0e4ed2416ea287e7
Ayu1104 <s1223138@cce.kanagawa-it.ac.jp>
2014-11-13 02:19:13 +0900

    analys_botton_tuika

--- android-sample/app/src/net/reduls/word/Sanmoku.java
+++ android-sample/app/src/net/reduls/word/Sanmoku.java
@@ -3,12 +3,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.KeyEvent;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.util.Log;
 import android.text.Html;
 import android.content.Intent;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.content.Context;
 import android.text.TextWatcher;
@@ -35,6 +37,17 @@ public class Sanmoku extends Activity implements TextWatcher
         getIntent().putExtra("search.key","");
         if(key!=null && key.equals("")==false)
             ((EditText)searchBar).setText(key);
+        
+        //ボタンクリック処理
+        //解析ボタン
+		Button analysbtn = (Button) this.findViewById(R.id.analysbtn);
+		analysbtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				analysbotton_OnClick(v);
+			}
+		});
+		//ボタンクリック処理ここまで
     }
 
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
@@ -54,4 +67,20 @@ public class Sanmoku extends Activity implements TextWatcher
             }
         }
     }
+    //解析ボタンをクリックしたとき
+	private void analysbotton_OnClick(View v) {
+        //String key = s.toString();
+        String key = "YO! SAY! 夏が胸を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋は やれ爽快 誤魔化し聞かない 薄着の曲線は 確信犯の しなやかなSTYLE 耐水性の 気持ちに切り替わる  瞬間のまぶしさは いかがなもの 心まで脱がされる 熱い風の誘惑に 負けちゃって構わないから 真夏は 不祥事も 君次第で 妖精たちが 夏を刺激する 生足魅惑のマーメイド 出すとこだして たわわになったら 宝物の恋が できそうかい 君じゃなくても バテ気味にもなるよ 暑いばっかの 街は憂鬱で スキを見せたら 不意に耳に入る サブいギャグなんかで 涼みたくない むせ返る熱帯夜を 彩る花火のように 打ち上げて散る思いなら  このまま抱き合って 焦がれるまで 妖精たちと 夏をしたくなる 熱い欲望はトルネイド 出すものだして 素直になりたい 君と僕となら it`s all right 都会のビルの上じゃ 感じなくなってる君を 冷えたワインの口づけで 酔わせて とろかせて 差し上げましょう 妖精たちが 夏を刺激する 生足ヘソ出しマーメイド 恋にかまけて お留守になるのも ダイスケ的にも オールオーケー YO! SAY! 夏を誰としたくなる 一人根の夜に you can say good bye 奥の方まで 乾く間ないほど 宝物の恋を しま鮮花";
+    	LinearLayout resultArea = (LinearLayout)findViewById(R.id.search_result_area);
+        resultArea.removeAllViews();
+        
+        if(key.length() > 0) { 
+            for(net.reduls.sanmoku.Morpheme e : net.reduls.sanmoku.Tagger.parse(key)) {
+                TextView txt = new TextView(this);
+                SpannableString spannable = DataFormatter.format("<"+e.surface+">\n"+e.feature);
+                txt.setText(spannable, TextView.BufferType.SPANNABLE);
+                resultArea.addView(txt);
+            }
+        }
+	}
 }
